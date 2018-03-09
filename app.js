86221ed7dc4850300f158b946bad19a06620cfcb
adoug <942434869@qq.com>
2018-03-09 16:40:29 +0800

    :pencil2: adjust route

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(logger('combined', {stream: accessLogStream}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/demo', express.static('public'));
 
 app.use('/', index);
 app.use('/demo', demo);
--- routes/demo.js
+++ routes/demo.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router
+  .get('/', function (req, res) {
+    res.render('index', { title: 'Express' })
+  })
   .get('/vueNightSwitch', function(req, res) {
-    res.render('vueNightSwitch', { layout: false });
+    res.render('vueNightSwitch', { layout: false })
   })
 
 module.exports = router
