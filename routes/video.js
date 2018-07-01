73318b3a03d141a814951d9272bebdd381cbf8a7
adoug <942434869@qq.com>
2018-07-02 00:23:57 +0800

    H5 video: 尝试chimee库

--- routes/video.js
+++ routes/video.js
@@ -5,4 +5,8 @@ router.get('/', function(req, res) {
   res.render('video', { layout: false });
 });
 
+router.get('/chimee', function(req, res) {
+  res.render('video_chimee', { layout: false });
+});
+
 module.exports = router;
